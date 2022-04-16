b9877533900a48d5c4954154fc6544dd002810aa
ChanakaDev <mr.mbcnb@gmail.com>
2022-04-16 19:52:15 +0530

    PY-2 - Formatted output

---
+++ 2_formatted_output.py
@@ -0,0 +1,15 @@
+'''
+formatted outputs
+=====================================================
+% <flag> <width> <.decimalPoints> <convertionType>
+'''
+print("%.2f" %(100.750)) #100.75 (2 decimal places)
+print("%.2f" %(200.250)) #200.25 (2 decimal places)
+print("%.2f" %(300.500)) #300.50 (2 decimal places)
+print("%.2f" %(1800.100)) #1800.10 (2 decimal places)
+
+print("%3d" %1) #  1 (3 places)
+print("%3d" %10) # 10 (3 places)
+print("%3d" %100) #100 (3 places)
+
+print("%4.2f" %(1800.100)) #1800.10 (4 places 2 decimal places) 
