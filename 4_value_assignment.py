80aadbd3757d4a9dc99dbc40c34ea691a9ab4807
ChanakaDev <mr.mbcnb@gmail.com>
2022-04-16 19:59:34 +0530

    PY-4 - Value assignment

---
+++ 4_value_assignment.py
@@ -0,0 +1,21 @@
+'''
+Value assignment for multiple variables
+=======================================
+'''
+num1 = 10
+num2 = 20
+num3 = 30
+print("\nnum 1: " + str(num1))
+print("num 2: " + str(num2))
+print("num 3: " + str(num3))
+
+
+num1, num2, num3 = 100,500,1500
+print("\nnum 1: " + str(num1))
+print("num 2: " + str(num2))
+print("num 3: " + str(num3))
+
+age1 = age2 = age3 = 25
+print("\nage 1: " + str(age1))
+print("age 2: " + str(age2))
+print("age 3: " + str(age3))
