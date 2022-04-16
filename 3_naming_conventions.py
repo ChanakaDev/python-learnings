fde417bed80a8c7200c2a2674386121c6be87170
ChanakaDev <mr.mbcnb@gmail.com>
2022-04-16 19:57:03 +0530

    PY-3 - Naming conventions

---
+++ 3_naming_conventions.py
@@ -0,0 +1,34 @@
+'''
+Naming Conventions
+=====================================================
+• Start with a letter or the underscore( _ ) character 
+• Cannot start with a number 
+• Variable names can only contain letters, numbers and underscores (A-z, 0-9, and _ ) 
+• Cannot contain special characters, except underscore
+• Reserved keywords cannot be used 
+• Use meaningful variable names
+'''
+
+'''
+Some correct variable namings
+------------------------------
+'''
+userName = "Kamal"
+_userName = "Kamal"
+
+user1name = "Kamal"
+user2Name = "Jayasundara"
+user_name = "Kamal"
+
+'''
+Some wrong variable namings
+------------------------------
+'''
+# 5userName = "Kamal"
+# userN@me = "Kamal"
+# user name = "Kamal"
+'''
+Not wrong, but not meaningfull
+------------------------------
+'''
+un = "Kamal" # not a meaningfull name
