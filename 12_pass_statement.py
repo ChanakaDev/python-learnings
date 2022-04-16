afda90be16857849dd4999a1ece8b72afa5f7340
ChanakaDev <mr.mbcnb@gmail.com>
2022-04-16 22:38:10 +0530

    PY-12 - Pass statement do nothing

---
+++ 12_pass_statement.py
@@ -0,0 +1,12 @@
+'''
+pass statement (do nothing)
+=====================================================
+    (*) placeholder with no action performed
+    (*) if you don't have a block of code to write when needed, you can simply put 'pass'
+'''
+
+number = int(input("enter a number: "))
+if number <= 100:
+    pass
+else:
+    print("number is greater than 100")
