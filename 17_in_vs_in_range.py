5ef91772e4077748217bec4cd426db2a4090363a
ChanakaDev <mr.mbcnb@gmail.com>
2022-04-17 00:18:52 +0530

    PY-17 - in vs in range

---
+++ 17_in_vs_in_range.py
@@ -0,0 +1,12 @@
+'''
+(*) "in range" is used only with numbers
+(*) "in" is used only with sequences(characters, list, tupe, dict..)
+'''
+# example 01:
+for number in range(1,11):
+    print(number)
+
+# example 02:
+letters = ['a','b','c','d','e','f','g','h']
+for letter in letters:
+    print(letter)
