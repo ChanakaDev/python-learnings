5679cf33fda22f3c6db19a355670600d66f58939
ChanakaDev <mr.mbcnb@gmail.com>
2022-04-17 00:10:11 +0530

    PY-15 - else with for loops

---
+++ 15_else_with_for_loop.py
@@ -0,0 +1,14 @@
+'''
+************************************
+else statement, with for loops
+************************************
+    (*) specifies a block of code to be executed, when the loop is finished
+    (*) same as the finnaly keyword in JS
+'''
+
+# print numbers 0-10 and finlly print "done"
+# ================================
+for i in range (11):
+    print(i)
+else:
+    print("done!")
