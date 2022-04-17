acc2723d14370af2af44b2cda2196c9638050094
ChanakaDev <mr.mbcnb@gmail.com>
2022-04-17 14:50:28 +0530

    Updated the readme file

---
+++ 20_lists.py
@@ -0,0 +1,35 @@
+# fruitList = ["Banana", "Apple", "Mango"]
+
+# # ordered: values can be accessed through indexes
+# # ----------------------------------
+# print(fruitList[0]) # Output: Banana
+# print(fruitList[1]) # Output: Apple
+# print(fruitList[2]) # Output: Mango
+
+# # mutable: values can be changed
+# # ----------------------------------
+# print(fruitList) # Output: ['Banana', 'Apple', 'Mango']
+# fruitList[0] = "Mangus"
+# print(fruitList) # Output: ['Mangus', 'Apple', 'Mango']
+
+# # allow duplicates
+# # ----------------------------------
+# fruitList = ["Banana", "Apple", "Mango", "Mango", "Mango"]
+# print(fruitList) # Output: ['Banana', 'Apple', 'Mango', 'Mango', 'Mango']
+
+# # length of the list
+# # ----------------------------------
+# print(len(fruitList)) # Output: 5
+
+# # different types of data can be hold
+# # ----------------------------------
+# list1 = [1,2,3,4,5]
+# list2 = ["item1", "item2", "item3", "item4"]
+# list3 = [True, True, False, False]
+
+# studentData = ["Kasun", 25, "Male", True]
+
+# make a list/ list constructor
+# ----------------------------------
+namesList = list(("name1", "name2", "name3", "name4")) 
+print(namesList)
